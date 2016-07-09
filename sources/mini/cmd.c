810928b06feedb9e124cdb27f43738807a146a59
cham-s <cattouma@student.42.fr>
2016-07-09 18:52:44 +0200

    search bin in hash

--- sources/mini/cmd.c
+++ sources/mini/cmd.c
@@ -6,7 +6,7 @@
 /*   By: cattouma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/02 10:43:16 by cattouma          #+#    #+#             */
-/*   Updated: 2016/07/09 16:39:55 by cattouma         ###   ########.fr       */
+/*   Updated: 2016/07/09 18:51:27 by cattouma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ static int		loop_paths(t_cmd *cmd, t_dict **dicts)
 			if (!access(exepath, F_OK))
 			{
 				cmd->exepath = ft_strdup(exepath);
+				dict_insert(dicts[BIN], cmd->av[0], cmd->exepath);
 				break ;
 			}
 			i++;
@@ -43,18 +44,27 @@ static int		loop_paths(t_cmd *cmd, t_dict **dicts)
 
 int				initcmd(t_dict **dicts, t_cmd *cmd, char **split_line)
 {
-	//char	*bin_key;
+	char	*bin_path;
 
 	cmd->exepath = NULL;
 	if (split_line == NULL)
 		exit(EXIT_FAILURE);
 	cmd->av = split_line;
 	cmd->ac = ft_tablen(cmd->av);
+	
+	// check if the condition below is really needed
 	if (cmd->av[0])
 	{
+		// strdup to bin_path?
+		if ((bin_path = dict_search(dicts[BIN], cmd->av[0])))
+		{
+			cmd->exepath = ft_strdup(bin_path);
+			return (0);
+		}
 		if (!access(cmd->av[0], F_OK))
 		{
 			cmd->exepath = ft_strdup(cmd->av[0]);
+			dict_insert(dicts[BIN], cmd->av[0], cmd->exepath);
 			return (0);
 		}
 		else
